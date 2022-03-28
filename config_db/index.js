d6a4ef8428eaa1c5fe25ddf4241476cc621e814d
Abhishek <abhishek.bhardwaj@tookitaki.com>
2022-03-28 13:51:05 +0530

    make it better

--- config_db/index.js
+++ config_db/index.js
@@ -11,15 +11,19 @@ let options = {
 
 
 
-( ()=>{
+  (() => {
 
-  if(process.env.NODE_ENV == 'production'){
-    options = {
+    if (process.env.NODE_ENV == 'production') {
+      options = {
+        host: "ec2-54-173-77-184.compute-1.amazonaws.com",
+        user: "uvozrtjqlqstft",
+        port: 5432,
+        password: "752c063a54283bb1902786d55491bd112723cbed185a5dc010645193a873a7a2",
+        database: "d6l8hmn3dbjvp3"
+      }
+    }
 
-        }
-  }
-
-} )();
+  })();
 
 const pool = new Pool(options);
 
