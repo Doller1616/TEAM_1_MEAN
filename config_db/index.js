8d6ddb1aa4b9a5e4e628239277ee16924b468661
Abhishek <abhishek.bhardwaj@tookitaki.com>
2022-03-28 13:42:44 +0530

    add heroku config

--- config_db/index.js
+++ config_db/index.js
@@ -1,13 +1,26 @@
 // https://node-postgres.com/features/connecting
 
 const { Pool } = require("pg");
+let options = {
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "admin",
+  database: "dolphin"
+}
 
-const pool = new Pool({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "admin",
-    database: "dolphin"
-  });
+
+
+( ()=>{
+
+  if(process.env.NODE_ENV == 'production'){
+    options = {
+
+        }
+  }
+
+} )();
+
+const pool = new Pool(options);
 
 module.exports = pool;
