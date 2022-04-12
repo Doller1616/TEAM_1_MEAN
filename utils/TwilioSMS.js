1cd648052e3b495c3a41d50efea0fd988e8c43b5
Abhishek <abhishek.bhardwaj@tookitaki.com>
2022-04-12 11:45:14 +0530

    Add tiwilio reference

--- utils/TwilioSMS.js
+++ utils/TwilioSMS.js
@@ -2,6 +2,7 @@ const accountSid = 'ACc168a321cff3d1ba7e028312e4cbc403';
 const authToken = 'take this token from account again "aa1..."';
 const client = require('twilio')(accountSid, authToken);
 
+//https://www.twilio.com/docs/sms/quickstart/node
 
 exports.sendMobileSMS = async ( body , to) => {
  return await client.messages .create({
