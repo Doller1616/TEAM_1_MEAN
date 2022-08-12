175c7aa8ea561ce18f314db9bee8fd9894666690
Abhishek <48003253+Doller1616@users.noreply.github.com>
2022-08-12 18:20:52 +0530

    ddd
    dddd

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 const server = require('./server');
 
-// const express = require('express');
-// const { db_connect,pool } = require('./db_config');
-// const { insertRecord } = require('./db_config/models');
-// const cors = require('cors');
-// const app = express();
+const express = require('express');
+const { db_connect,pool } = require('./db_config');
+const { insertRecord } = require('./db_config/models');
+const cors = require('cors');
+const app = express();
 
-// app.get('/', async (req, res) => {
-//   res.send('server Started')
-// });
+app.get('/', async (req, res) => {
+  res.send('server Started')
+});
 
-// app.get('/create', async (req, res) => {
-//   pool.query('SELECT * FROM student')
-//     .then(r => res.send(r.rows))
-//     .catch(e => { res.send({ error: e }); console.log(e); })
-// });
+app.get('/create', async (req, res) => {
+  pool.query('SELECT * FROM student')
+    .then(r => res.send(r.rows))
+    .catch(e => { res.send({ error: e }); console.log(e); })
+});
 
 
-// app.get('/list', async (req, res) => {
-//   pool.query('SELECT * FROM student')
-//     .then(r => res.send(r.rows))
-//     .catch(e => { res.send({ error: e }); console.log(e); })
-// });
+app.get('/list', async (req, res) => {
+  pool.query('SELECT * FROM student')
+    .then(r => res.send(r.rows))
+    .catch(e => { res.send({ error: e }); console.log(e); })
+});
 
-// app.post('/add', async (req, res) => {
-//   const { firstname, lastname, age, address, email } = req.body;
-//   pool.query("INSERT INTO student(firstname,lastname,age,address,email) VALUES ($1,$2,$3,$4,$5)",
-//     [firstname, lastname, age, address, email])
-//     .then(r => res.send({ data: req.body, msg: 'added successfully' }))
-//     .catch(e => { res.send({ error: e }); console.log(e); })
-// });
+app.post('/add', async (req, res) => {
+  const { firstname, lastname, age, address, email } = req.body;
+  pool.query("INSERT INTO student(firstname,lastname,age,address,email) VALUES ($1,$2,$3,$4,$5)",
+    [firstname, lastname, age, address, email])
+    .then(r => res.send({ data: req.body, msg: 'added successfully' }))
+    .catch(e => { res.send({ error: e }); console.log(e); })
+});
 
 app.get('/delete/:id', (req, res) => {
   const { id } = req.params;
